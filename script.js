a31b0b82f00713683b9e167bbd2c103e93fdb8c7
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-04-20 20:35:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 function validEmail(str) {
-  //your JS code here.
-}
+	//your JS code here.
+	  let splitMail1 = str.split("@");
+	  if(splitMail1.length<2) {
+		  return false;
+	  }
+
+	  if(splitMail1[0]=="") {
+		return false;se
+	  }
+	  
+	  let secondHalf = splitMail1[1];
+	  let splitMail2 = secondHalf.split('.');
+
+	  if(splitMail2.length==3) {
+		if(splitMail2[1]!="com") {
+			return false;
+		}
+		if(splitMail2[2]!="in") {
+			return false;
+		}
+	  }
+
+	  if(splitMail2.length<2) {
+		  return false;
+	  }
+  
+	  let half2 = splitMail2[1];
+	  if(half2!='com') {
+		  return false;
+	  }
+	  return true;
+	  
+	  
+  }
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
